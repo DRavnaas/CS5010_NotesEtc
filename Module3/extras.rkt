7f6e2016e952c24d70d877c44f6778d2b573a339
DRavnaas <doylerav@gmail.com>
2014-01-30 18:51:51 -0800

    saving module 3 work

--- Module3/extras.rkt
+++ Module3/extras.rkt
@@ -2,17 +2,25 @@
 
 (provide provide rename-out struct-out check-error)
 
+(provide display)
+
 (require rackunit)
-; (require rackunit/text-ui)
-(require (only-in rackunit/text-ui [run-tests run-tests-orig]))
+(require rackunit/text-ui)
 
 (define-syntax (check-error stx)
   (syntax-case stx ()
     [(_ code . msg) #`(check-exn exn:fail? (lambda () code) . msg)]))
 
-(define-syntax (run-tests stx)
-  (syntax-case stx ()
-    [(_ suite-name)
-     #`(begin
-         (printf 'suite-name "~d~%")
-         (run-tests-orig suite-name))]))
+;; for Problem Set 6, render-expr problem
+
+;; ListOf<String> -> Void
+;; GIVEN: A List of Strings
+;; EFFECT: displays the strings in separate lines
+;; RETURNS: the empty string
+(define (display-strings! strs)
+  (if (empty? strs) (void)
+      (let* ((d1 (display (first strs)))
+             (d2 (display "\n")))
+        (display-strings! (rest strs)))))
+
+(provide display-strings!)
