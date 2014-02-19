1fe45a8edba9f03db2df0641093ea4f8e9c594a8
DRavnaas <doylerav@gmail.com>
2014-02-19 09:29:51 -0800

    worked on rendering nodes in a scene - got an initial cut working

--- Stephen/scene-and-nodes.rkt
+++ Stephen/scene-and-nodes.rkt
@@ -25,60 +25,90 @@
   50 50 
   scene-with-one-rectangle))
 
-; two rectangles with a line
+; An example of drawing a line between two rectangles
 ;(scene+line scene-with-two-rectangles
 ;            50 50
 ;            100 100
 ;            "blue") 
 
-;or we could use place-images for all the rectangles?
+; Note to Steve - 
+; There's an alternative of using place-images, rather than place-image;
+; it takes a list of images and a list of positions.  It means we'd
+; have to write a function to gather up a list of rectangles and positions
+; from our roots and then pass that in "at the top".  
+; Somehow that seems like it doesn't follow the recursive
+; pattern quite as well, so I'm instead building a composite image with a
+; place-image call for each node.
 
-(define-struct node (x y branch))
+; Simplified data definitions for just working on scene
+; TODO - add selected? and node-child-fit? handling
+(define-struct node (x-pos y-pos branch))
+(define-struct world (roots))
+; roots is a ListOf<Node>
 
-(define-struct world (nodes))
-
+; examples for manual testing
 (define empty-world (make-world empty))
-
 (define world-with-one-node (make-world (list (make-node 20 20 empty))))
-
 (define world-with-two-nodes (make-world 
                              (list (make-node 20 20 empty)
-                                   (make-node 100 100 empty)
-                             )))
-
+                                   (make-node 100 100 empty))
+                             ))
 
 (define world-with-three-nodes (make-world 
-                             (list (make-node 20 20 
-                                              (list (make-node 40 40 empty)))
-                                   (make-node 100 100 empty)
-                             )))
+                                (list (make-node 20 20 
+                                        (list (make-node 40 40 empty)))
+                                      (make-node 100 100 empty)))
+                             )
+(define world-with-two-roots-with-branches 
+  (make-world 
+      (list 
+       ; first root - two children, two grandchildren
+       (make-node 20 20 (list (make-node 40 40 empty)                               
+                              (make-node 200 200 (list (make-node 250 250 empty)
+                                                       (make-node 100 100 empty)))))
+       ; second root - no grandchildren     
+       (make-node 300 50 empty)))
+      )
 
+; functions
 
+; STRATEGY: Structural decomposition on World
 (define (world-to-scene w)
-  (nodes-to-scene w EMPTY-CANVAS))
+  (nodes-to-scene 
+   (world-roots w)
+   EMPTY-CANVAS))
 
-; not working with three node 
-(define (nodes-to-scene w image)
-  ; Fold each set of nodes into an image
-  (foldr nodes-to-image image (world-nodes w)))
-
-; This should use the template, not working on three node version
-(define (nodes-to-image w-node image)
- (cond 
-   ((empty? w-node) image)
-   ((node? w-node) (node-to-image w-node image))
-   (else (place-image 
-          UNSELECTED-NODE
-          (node-x w-node)
-          (node-y w-node)
-          (nodes-to-image (node-branch w-node) image)))))
+; STRATEGY: HOFC 
+(define (nodes-to-scene w-nodes image)
+  ; Fold each root (and its subtree) into an image
+  (foldr node->image image w-nodes))
 
 
-(define (node-to-image a-node image)
+; STRATEGY: Structural decomposition on Node
+(define (node->image a-node image)
+  (place-node-on-image 
+        (node-x-pos a-node)
+       (node-y-pos a-node)
+       ;(node-selected? a-node)
+       ;(node-child-fit? a-node)
+       
+       ; build image from rest of branch
+       (branch->image (node-branch a-node) image)))
+
+; STRATEGY: Structural decomposition on Branch
+(define (branch->image a-branch image)
+  (cond
+    [(empty? a-branch) image]
+    [else (node->image (first a-branch)
+                       ; Image passed into node->image is the one 
+                       ; built for all the branch nodes
+                       (branch->image (rest a-branch) image))]))
+
+; STRATEGY: Domain Knowledge
+(define (place-node-on-image x y image)
   (place-image 
        UNSELECTED-NODE
-       (node-x a-node)
-       (node-y a-node)
+       x y
         image))
 
 ;(world-to-scene (make-world empty))
@@ -89,5 +119,6 @@
 
 (world-to-scene world-with-two-nodes)
 
-; This one isn't right yet - not recursing subtree
 (world-to-scene world-with-three-nodes)
+
+(world-to-scene world-with-two-roots-with-branches)
